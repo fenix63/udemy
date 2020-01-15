c204c3f3c08c1fa2d2a9a5eb6f7cf3b76d371656
Iurii Maratkanov <youran_88>
2020-01-15 23:18:33 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+      WarningalertComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
